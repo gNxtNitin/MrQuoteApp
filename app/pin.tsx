2d1a5a4b645d48d1983e9e9e1e5187b92a122f98
Gokul2803 <bhattgokul04@gmail.com>
2024-12-26 12:17:57 +0530

    pin and face auth page

--- app/pin.tsx
+++ app/pin.tsx
@@ -1,5 +1,5 @@
 import { PinScreen } from "./components/features/pin/PinScreen";
 
 export default function PinPage() {
-  return <PinScreen />;
+  return <PinScreen onPin={() => {}} isDarkMode={false} />;
 }
